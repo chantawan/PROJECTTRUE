f3941fff6c1d6bca49ef2eba0a3e9adef0228f39
chantawan <love-za60@hotmail.com>
2022-03-08 14:44:41 +0700

    gg

--- document_recive_user.php
+++ document_recive_user.php
@@ -1,119 +1,113 @@
-<?php
-include "connect.php";
-
-if(!isset($_SESSION['emp_id'])){
-   header('Location: login_user.php');
-}
-
-$emp_id = $_SESSION['emp_id'];
-$emp_firstname = $_SESSION['emp_firstname'];
-$Position_name = $_SESSION['Position_name'];
-
-date_default_timezone_set("Asia/Bangkok");
-
-?>
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
-  <link rel="icon" href="img/logo.png" type="image/png">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <script src="js/bootstrap.bundle.min.js"></script>
-  <script src="js/jquery-3.5.1.min.js"></script>
-  <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <style>
-    * {
-      font-family: 'supermarket';
-    }
+  <title>เข้าสู่ระบบสมาชิก</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
 
-    body {
-      background-image: url('img/background.jpg');
-      background-repeat: no-repeat;
-      background-position: center center;
-      background-attachment: fixed;
-      background-size: 100% 100%, auto;
-    }
-    .clear {
-      clear: both;
-    }
-    .navbar{
-      position:fixed;
-      width:100%;
-      z-index:1;
-    }
-    .test{
-      background-image: url('img/paper.png');
-      background-repeat: no-repeat;
-      background-position: center center;
-      /* background-attachment: fixed; */
-      background-size: 80% 80%, auto;
-      height:500px;
-      padding-top:100px;
-      padding-left:150px;
-      font-size:180px;
-      -webkit-text-stroke: 2px white;
-    }
-    .shadowx{
-      box-shadow:  8px 8px rgba(0, 0, 0, 0.25);
-      border:2px solid #F3E3D9;
-    }
-    .bgcon2{
-        background-color: rgba(255,255,255,0.7);
-        box-shadow: 0 0px 10px 0 rgb(0,0,0);
-    }
+  <!-- Favicons -->
+  <link rel="icon" href="img/login2.png" type="image/png">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
-  </style>
-  <title>หน้าแรก</title>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="width:100%">
-    <div class="container-fluid">
-      <img src=""  width="2%" alt=""><a class="navbar-brand" href="index_user.php"> &nbsp</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-link" aria-current="page" href="index_user.php">หน้าแรก</a>
-          <a class="nav-link" href="document_recive.php">เอกสารถึงตัวท่าน</a>
-          <a class="nav-link" href=".php">แฟ้มเอกสาร</a>
-          <a class="nav-link" href=".php">คู่มือ</a>
-          <a class="nav-link" href="logout.php?option=2">ออกจากระบบ</a>     
-        </div>   
-      </div>
-      <div style="text-align:right; float:right;">
-        <label style="color:#FFFFFF83">ชื่อผู้ใช้ : <?php echo $emp_firstname ?> &nbsp</label>
-        <label style="color:#FFFFFF83">บทบาท : <?php echo $Position_name ?> &nbsp</label>
-      </div>
-    </div>
-  </nav>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  <link rel="icon" href="img/logoUser1.png" type="image/png">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+  <!-- =======================================================
+  * Template Name: DevFolio - v4.7.1
+  * Template URL: https://bootstrapmade.com/devfolio-bootstrap-portfolio-html-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
 </head>
-<body>
-  <div class="container btn-success" style = "backgroud-color:back; width:11%; height:80px;
-  padding: 0.5%;border: 3px solid black; text-align: ;" >
-    <div class="col">
-        <?php
-        $sql_query = "SELECT COUNT(document_id) FROM document;
-        where emp_id = '$emp_id'";
-        $result = mysqli_query($conn,$sql_query);
-        ?>
-      เอกสารที่ยังไม่ได้อ่าน
-      
-    </div>
-    
-    </div>
-    
 
-    
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center justify-content-between">
+
+    <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto  " href="index_user2.php">หน้าแรก</a></li>
+          <li><a class="nav-link scrollto active" href="document_recive_user.php">เอกสารถึงตัวท่าน</a></li>
+          <li><a class="nav-link scrollto" href="#">แฟ้มเอกสาร</a></li>
+          <li><a class="nav-link scrollto" href="#">คู่มือ</a></li>
+          <li><a class="nav-link scrollto" href="logout.php?option=2">ออกจากระบบ</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+      
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <div id="hero" class="hero route bg-image" style="background-image: url(assets/img/wall.jpg)">
+    <div class="overlay-itro"></div>
+    <div class="hero-content display-table">
+      <div class="table-cell">
+        <div class="container">
+        
+                
+                   
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+          <!--<p class="display-6 color-d">Hello, world!</p>-->
+          <!-- <p class="pt-3"><a class="btn btn-primary btn js-scroll px-4" href="#about" role="button">Learn More</a></p> -->
+        </div>
+      </div>
+    </div>
+  </div><!-- End Hero Section -->
+
+  </main><!-- End #main -->
+  
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/typed.js/typed.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
 </body>
 
 </html>
+
+
+        
--- upload.php
+++ upload.php
@@ -15,7 +15,7 @@
 </head>
 
 <?php
-$conn=new PDO('mysql:host=localhost; dbname=project', 'root', '') or die(mysql_error());
+$conn=new PDO('mysql:host=localhost; dbname=upload', 'root', '') or die(mysql_error());
 if(isset($_POST['submit'])!=""){
   $name=$_FILES['file']['name'];
   $size=$_FILES['file']['size'];
@@ -44,7 +44,7 @@ if(isset($_POST['submit'])!=""){
  if($move){
  	$query=$conn->query("insert into upload(name,fname)values('$name','$fname')");
 	if($query){
-	header("location:index.php");
+	header("location:upload.php");
 	}
 	else{
 	die(mysql_error());
